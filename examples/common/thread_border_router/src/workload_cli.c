496df0c5ac519238b813f60d15d7ade73e85353c
Simeon Tran <siatran@ucsc.edu>
2024-04-12 12:12:01 -0700

    saving what I have so far.

--- examples/common/thread_border_router/src/workload_cli.c
+++ examples/common/thread_border_router/src/workload_cli.c
@@ -1,7 +1,14 @@
 #include "workload.h"
 
-static otCoapResource *aPeriodicResource;
+/**
+ * Will receive packets of Scenario 1.
+*/
 static otCoapResource *periodicResource;
+
+/**
+ * Will receive packets of Scenario 2.
+*/
+static otCoapResource *aPeriodicResource;
 
 void startCoapServer(uint16_t port) {
   otError error = otCoapStart(OT_INSTANCE, port);
@@ -22,18 +29,18 @@ otError expServerStart(void* aContext, uint8_t argsLength, char* aArgs[])
   /**
    * Allocate HEAP Memory for resource that receive packets from Scenario 1.
   */
-  aPeriodicResource = calloc(1, sizeof(otCoapResource));
-  createAPeriodicResource(aPeriodicResource);
-  otCoapAddResource(OT_INSTANCE, aPeriodicResource);
-  otLogNotePlat("Set up resource URI: '%s'.", aPeriodicResource->mUriPath);
-
-  /**
-   * Allocate HEAP Memory for resource that receive packets from Scenario 2.
-  */
   periodicResource = calloc(1, sizeof(otCoapResource));
   createPeriodicResource(periodicResource);
   otCoapAddResource(OT_INSTANCE, periodicResource);
   otLogNotePlat("Set up resource URI: '%s'.", periodicResource->mUriPath);
+
+  /**
+   * Allocate HEAP Memory for resource that receive packets from Scenario 2.
+  */
+  aPeriodicResource = calloc(1, sizeof(otCoapResource));
+  createAPeriodicResource(aPeriodicResource);
+  otCoapAddResource(OT_INSTANCE, aPeriodicResource);
+  otLogNotePlat("Set up resource URI: '%s'.", aPeriodicResource->mUriPath);
 
   return OT_ERROR_NONE;
 }
