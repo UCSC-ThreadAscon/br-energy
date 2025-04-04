1cae67795a6ebed6c0f9238c7e4e142047e0a611
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-03 21:56:59 -0700

    Add IP address of SED

--- examples/common/thread_border_router/src/stats.c
+++ examples/common/thread_border_router/src/stats.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-#define ADDRESS_SED_1 "fd84:7733:23a0:f199:87c3:4196:6d07:3a5f"
+#define ADDRESS_SED_1 "fd07:dd09:5dd8:2c0c:6490:4dc4:8e42:bf0d"
 #define ADDRESS_SED_2 "fd84:7733:23a0:f199:cd17:19f6:4135:f55b"
 #define ADDRESS_SED_3 "fd84:7733:23a0:f199:c257:afca:b5f2:1452"
 
