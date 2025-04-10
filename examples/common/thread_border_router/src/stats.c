d636e7310c8fbaefc72970b823f4ce4b102bd905
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-10 16:40:58 -0700

    IP address of front door

--- examples/common/thread_border_router/src/stats.c
+++ examples/common/thread_border_router/src/stats.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-#define ADDRESS_SED_1 "fd07:dd09:5dd8:2c0c:9792:c676:49f5:d325"
+#define ADDRESS_SED_1 "fdd8:91f7:3002:2147:9e4d:da6b:6b9c:8dd1"
 #define ADDRESS_SED_2 "fd07:dd09:5dd8:2c0c:1ffc:5e2a:b7fd:7243"
 #define ADDRESS_SED_3 "fd07:dd09:5dd8:2c0c:87c3:4196:6d07:3a5f"
 
